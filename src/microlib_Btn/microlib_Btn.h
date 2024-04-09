669eca8bbe969a05260d8aff4582a4521b99f0fe
Roch Schanen <48413159+RochSchanen@users.noreply.github.com>
2024-04-09 22:12:31 +0100

    Update comments in microlib_Btn.h

--- src/microlib_Btn/microlib_Btn.h
+++ src/microlib_Btn/microlib_Btn.h
@@ -7,20 +7,31 @@
 #include "microlib.h"
 
 /*
-	Button micro library: setup() defines the digital input
-	pin. A refreshing rate at 0.100s maximum is used to
-	prevent	switch rebounce triggers. updt() updates the
-	buttons	status: wether it has been pressed or not.
-	It has to be called on a regular basis. pressed()
-	returns 1 if the button has been pressed (digital fall).
-	pressed() clears the pressed flag. released() returns 1 
-	if the button has been released (digital rise). pressed()
-	clears the released flag.
+	This defines the Button class of the microlib library.
+ 	
+  	- the setup() method defines the digital input pin.
+  	A refreshing rate of 0.100s is used by default to
+   	prevent any possible spurious triggers due to
+    	unexpected mechanical rebounce of the switch.
+     
+    	- the updt() method updates the buttons state values,
+     	i.e. whether it has been pressed, released, or not. It
+      	has to be called on a regular basis.
+  	
+   	- the pressed() method returns 1  (True) if the button
+   	has been pressed (digital fall) since the previous call.
+    	When called, the pressed() method clears the "pressed" flag.
+     
+     	- the released() method returns 1 (True) if the button
+      	has been released (digital rise) since the previous call.
+       	When called, the released() method clears the "released" flag.
 
 	dependences: This library uses the micro library "microlib_Tmr".
+ 	The #include "microlib.h" at the top of this file should take
+  	care of loading "microlib_Tmr" if necassary.
 	
 	todo:
-	1) optimize the code by bypassing digitalRead().
+	1) optimize the code by bypassing the standard "digitalRead()".
 	2) combine button updates by reading a pin port only once:
 		m = (1<<p);
 		s = (PIND & m) >> p;
@@ -31,14 +42,14 @@ class microlib_Btn{
 
 	private:
 
-		// uint8_t m; 	// mask
+		// uint8_t m; 	// mask (not yet implemented)
 
 		uint8_t p; 			// pin
 		uint8_t s; 			// pin status
 		uint8_t q; 			// button pressed status
 		uint8_t r; 			// button released status
 		uint8_t i; 			// readout value
-		microlib_Tmr  t;	// timer
+		microlib_Tmr  t;		// timer
 
 	public:
 
