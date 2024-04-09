428340c1b92bc03b81356e8bc8755406ff684ca6
Roch Schanen <48413159+RochSchanen@users.noreply.github.com>
2024-04-09 22:47:38 +0100

    Update comments in microlib_DsB.h

--- src/microlib_DsB/microlib_DsB.h
+++ src/microlib_DsB/microlib_DsB.h
@@ -8,23 +8,24 @@
 
 /* This is a convoluted way of writing text to the SSD1306 display that
  * prevents delaying the main loop. You setup a buffer to be displayed
- * on the SSD1306. The text to display is written to the buffer string.
- * calls to updt() will step by step go through all the buffer to be
- * displayed. To change the text to display, simply re-write in the
- * string buffer (do not forget the end-of-string char).
+ * on the SSD1306. The text to display is written to a buffer string.
+ * Regular calls to the updt() method will go through the buffer string
+ * character by character and send them to the display one by one.
+ * To update the text to display, simply re-write in the buffer string
+ * (do NOT forget the end-of-string char).
  */
 
 // maximum buffer length
-// (SSD1306 has a 16x8 columns)
-#define DSB_BUFLEN 16
+// (the SSD1306 display has a maximum of 16 columns with 8 pixels characters width)
+#define DSB_BUFLEN 16 
 
 class microlib_DSB{
 
     private:
 
-        microlib_SSD1306 *d;                 // SSD1306 display
-        uint8_t x, y;                  // buffer position is static
-        static char b[DSB_BUFLEN + 1]; // transfer buffer: must also include the end-of-string
+        microlib_SSD1306 *d;           // SSD1306 display
+        uint8_t x, y;                  // buffer position (fixed)
+        static char b[DSB_BUFLEN + 1]; // transfer buffer (must also include the end-of-string char)
         static uint8_t n;              // stack size of diplay buffers: common to all instances
         static uint8_t i;              // the current display buffer id: common to all instances
         uint8_t j, k;                  // j is the current instance identifier, k is a temp var
