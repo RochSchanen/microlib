38097081547a3902a617054f32de834b8215f183
Roch Schanen <48413159+RochSchanen@users.noreply.github.com>
2024-04-09 22:28:47 +0100

    Update comment in microlib_Btn.cpp

--- src/microlib_Btn/microlib_Btn.cpp
+++ src/microlib_Btn/microlib_Btn.cpp
@@ -2,10 +2,13 @@
 #include "microlib.h"
 
 int16_t microlib_Btn::setup(uint8_t pin){
+	// setup pin
 	p = pin; pinMode(p, INPUT_PULLUP);
+	// setup initial state values
 	s = digitalRead(p); q = 0; r = 0;
-	t.setup(100000); // 100ms
-	t.reset();
+	// setup anti-rebounce timer
+	t.setup(100000); 	// use 100ms by default
+	t.reset(); 		// reset timer
 	return 0;
 }
 
@@ -13,35 +16,37 @@ int16_t microlib_Btn::updt(void){
 	// wait for timer
 	if (t.ready()){
 		i = digitalRead(p);
-		// change of pin state?
-		if (i == s) return 0;
+		// has the pin changed its state?
+		if (i == s) return 0; // no -> done
+		// yes -> update flags
 		switch(i){
-			case 0: // button pressed
-				q = 1;
+			case 0: // the button has been pressed
+				q = 1; // set the pressed flag
 				break;
-			case 1: // button released
-				r = 1;
+			case 1: // the button has been released
+				r = 1; // set the released flag
 				break;
 		}
 		// update pin state
 		s = i; 
-		// set another delay
+		// reset timer
 		t.reset();
 	}
+	// done
 	return 0;
 }
 
 int16_t microlib_Btn::pressed(void){
 	if(q){
-		q = 0; // the call clears the pressed status
-		return 1;
+		q = 0; // clear the pressed flag when called
+		return 1; 
 	}
 	return 0;
 }
 
 int16_t microlib_Btn::released(void){
 	if(r){
-		r = 0; // the call clears the released status
+		r = 0; // clear the released flag when called 
 		return 1;
 	}
 	return 0;
